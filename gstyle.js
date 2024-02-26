3e31973ec13315eb830dfbf4d2648b5d151b38e6
mrhanafi <muhdhanafirahman@gmail.com>
2024-02-26 11:00:51 +0800

    initial config homescreen

---
+++ gstyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+
+const s = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f9f9f9'
+    },
+    header: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    body: {
+        flex: 5,
+        backgroundColor: 'blue'
+    },
+    footer: {
+        height: 70,
+        backgroundColor: 'yellow',
+    },
+});
+
+export default s;
